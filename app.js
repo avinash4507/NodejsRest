59c36b759765ea94b192bc47b87f5e6a36f114ad
avinash <avinash4507@gmail.com>
2016-05-01 12:25:45 +0530

    setup express router, mongoose and its schema

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 var express = require('express');
 
+var mongoose = require('mongoose');
+
+var db = mongoose.connect('mongodb://localhost/booksAPI');
+
+var Book = require('./models/bookModel');
+
 var app = express();
 
 var port = process.env.PORT || 3000;
+
+var myRouter = express.Router();
+
+myRouter.route('/Books')
+    .get(function(req, res){
+        Book.find(function(err, books){
+            if(err)
+                res.status(500).send(err);
+            else
+                res.json(books);
+        });
+    })
+
+app.use('/api', myRouter);
 
 app.get('/', function(req, res) {
     res.send('Welcome to my Node and Express app!!');

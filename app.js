67f61c7527223572063c17437a47889c7125aba1
avinash <avinash4507@gmail.com>
2016-05-01 11:15:55 +0530

    initial gulp and nodemon setup

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 var express = require('express');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res) {
+    res.send('Welcome to my Node and Express app!!');
+});
+
+app.listen(port, function() {
+    console.log('App listening on port ' + port + '!');
+});
